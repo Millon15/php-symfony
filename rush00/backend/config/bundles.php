ef3e7d95b7fb213beda00e3ba6229ca20c066d46
vbrazas <vbrazas@student.unit.ua>
2020-03-02 21:20:05 +0200

    Git repo refactored

--- rush00/backend/config/bundles.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
-    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
-    Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
-];
